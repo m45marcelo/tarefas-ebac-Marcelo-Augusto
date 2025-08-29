de62454d38e37cf3960c2c36ba57d69ac7ec1083
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-29 19:18:51 -0300

    criado a classe Calculo Media

---
+++ mod8/TarefaCalculoMedia/src/CalculoMedia.java
@@ -0,0 +1,12 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+        double nota1 = 6.5;
+        double nota2 = 8.0;
+        double nota3 = 5.0;
+        double nota4 = 7.0;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(media);
+    }
+}
