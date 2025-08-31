9d329dcfbaf661a6072a9be5bf906b86702fd982
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-08-31 00:17:38 -0300

    criado a classe TarefaWrapper

---
+++ mod9/Tarefa_Wrapper/src/TarefaWrapper.java
@@ -0,0 +1,8 @@
+public class TarefaWrapper {
+    public static void main(String[] arg){
+    double primitivoDouble = 3.0;
+    System.out.println(primitivoDouble);
+    Double wrapperDouble = primitivoDouble;
+    System.out.println(wrapperDouble);
+    }
+}
